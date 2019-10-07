0a4eaa34c6558321998dcb765d7a1a33351d9d94
Ailyinchick <Ailyinchick@gmail.com>
2019-10-07 18:01:34 +0300

    add buttons for project; need to add logic in theese buttons

--- src/main/java/net/proselyte/springmvc/controller/DeveloperController.java
+++ src/main/java/net/proselyte/springmvc/controller/DeveloperController.java
@@ -5,17 +5,16 @@ import net.proselyte.springmvc.DAO.DAOuser;
 import net.proselyte.springmvc.model.Developer;
 import org.springframework.stereotype.Controller;
 
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class DeveloperController {
     DAOAccount da = new DAOAccount();
     DAOuser du = new DAOuser();
+
     @GetMapping(value = "/")
     public String index() {
         return "/index";
@@ -29,8 +28,32 @@ public class DeveloperController {
 
     @GetMapping(value = "findRichest")
     public String mymeth1(ModelMap model) {
-        //model.addAttribute("dev", Developer.create("Mike").getName());
-        model.addAttribute("dev", da.findRichest().getName());
+        model.addAttribute("richestUser", Developer.create("Mike").getName());
+       // model.addAttribute("dev", da.findRichest().getName());
+        return "index";
+    }
+
+    @GetMapping(value = "findAll")
+    public String findAll(ModelMap model) {
+        List varList = du.findAll();
+/*        for (int i = 0; i < 2; i++) {
+
+        }*/
+        model.addAttribute("allUsers"," varList.get(2).toString()");
+        return "index";
+    }
+
+
+    @GetMapping(value = "bankSumm")
+    public String bankSumm(ModelMap model) {
+
+        return "index";
+    }
+
+
+    @GetMapping(value = "findById")
+    public String findById(ModelMap model) {
+
         return "index";
     }
 
--- src/main/webapp/WEB-INF/pages/index.jsp
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -5,14 +5,31 @@
     <title>Home Page</title>
 </head>
 <body>
-<form action="myAction">
-    <input type="submit" value="Say hello">
+<form action="/myAction">
+    <input type="submit" value="Hello from spring">
     <h1> ${attrib} </h1>
 </form>
 
-<form action="findRichest">
+
+<form action="/findById">
+    <input type="text" name="userID"/>
+    <input type="submit" value="Find by ID">
+    <h1> ${userById} </h1>
+</form>
+
+<form action="/findRichest">
     <input type="submit" value="Show Richest">
-    <h1> ${dev} </h1>
+    <h1> ${richestUser} </h1>
+</form>
+
+<form action="/findAll">
+    <input type="submit" value="Display all">
+    <h1> ${allUsers} </h1>
+</form>
+
+<form action="/bankSumm">
+    <input type="submit" value="Bank summ">
+    <h1> ${bankSumm} </h1>
 </form>
 
 </body>
